62bfbea18e29ccb23ef3ad497c6c18a3d35fc4dc
ReedyCloud <bartekciach1@gmail.com>
2022-12-16 00:53:33 +0100

    add: omit, returnType, readonlyByKeys

---
+++ medium/readonly-by-keys.ts
@@ -0,0 +1,35 @@
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [key in keyof T as key extends K ? key : never]: T[key];
+} & {
+  [key in keyof T as key extends K ? never : key]: T[key];
+};
+
+/* _____________ Test Cases _____________ */
+import type { Alike, Expect } from "../utils.ts";
+
+type cases = [
+  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+  Expect<Alike<MyReadonly2<Todo1, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "title" | "description">, Expected>>
+];
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, "title" | "invalid">;
+
+interface Todo1 {
+  title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Todo2 {
+  readonly title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Expected {
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
+}
